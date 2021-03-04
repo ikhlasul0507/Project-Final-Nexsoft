797fc11bd8a2cb4c9e1a7e9142dfe94b0b3c69b1
Ikhlasul Amal <ikhlasul0507@gmail.com>
2021-03-04 10:09:20 +0700

    first create project and dependency

---
+++ src/test/java/com/example/projectfinal/projectfinal/ProjectfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectfinal.projectfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectfinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
