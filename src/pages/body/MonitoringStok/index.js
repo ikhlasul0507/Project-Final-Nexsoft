4930eaa8aa0c43eb5e228b48bf55595f14040b7d
Ikhlasul Amal <ikhlasul0507@gmail.com>
2021-03-15 13:52:37 +0700

    fixs module pagination and show data

--- src/pages/body/MonitoringStok/index.js
+++ src/pages/body/MonitoringStok/index.js
@@ -43,14 +43,28 @@ class MonitoringStok extends Component {
             stoks: [],
             url: "http://localhost:8080/nd6/stock/",
             detail: false,
+            errorFetcing: false,
             id: "",
-            checkedA: true
+            page: 1,
+            count: "",
+            orderby: "asc",
+            show: "7",
+            checkedA: true,
+            minus: 0,
+            name: ""
         }
         this.handleChangeMinus = (event) => {
             this.setState({
                 [event.target.name]: event.target.checked,
             })
-        };
+        }
+        this.handleChange = (event, value) => {
+            this.setState({
+                page: value
+            })
+            // this.getCount();
+            this.getPaging(value, this.state.orderby, this.state.show, this.state.minus, this.state.name);
+        }
         this.detail = idStok => {
             this.setState({
                 detail: true,
@@ -63,31 +77,17 @@ class MonitoringStok extends Component {
                 id: ""
             })
         }
-        this.getApiStock = () => {
-            fetch(this.state.url, {
-                method: "get",
-                headers: {
-                    "Content-Type": "application/json; ; charset=utf-8",
-                    "Access-Control-Allow-Headers": "Authorization, Content-Type",
-                    "Access-Control-Allow-Origin": "*"
-                },
+        this.onChangeSelectShow = el => {
+            const show = el.target.value
+            this.setState({
+                show: show
             })
-                .then(response => response.json())
-                .then((json) => {
-                    this.setState({
-                        stoks: json
-                    })
-                    console.log("Stocks :", this.state.stoks);
-                })
-                .catch((e) => {
-                    console.log(e)
-                    alert("Failed sending data!!");
-                });
-
+            this.getPaging(1, this.state.orderby, show, this.state.minus, this.state.name);
+            this.getCount();
         }
         this.delete = (idStok) => {
             // if (window.confirm('Are you sure wont to Delete ?')) {
-                console.log("id stok :", idStok)
+            console.log("id stok :", idStok)
             swal({
                 title: "Are you sure?",
                 text: "wont to Delete ?",
@@ -124,7 +124,7 @@ class MonitoringStok extends Component {
                                         timerProgressBar: true
                                     })
                                     this.getApiStock()
-                            }
+                                }
                             })
                             .catch((e) => {
                                 alert("Failed fetching data!!", e)
@@ -136,16 +136,87 @@ class MonitoringStok extends Component {
                 });
 
         }
+        this.getApiStock = () => {
+            fetch(this.state.url, {
+                method: "get",
+                headers: {
+                    "Content-Type": "application/json; ; charset=utf-8",
+                    "Access-Control-Allow-Headers": "Authorization, Content-Type",
+                    "Access-Control-Allow-Origin": "*"
+                },
+            })
+                .then(response => response.json())
+                .then((json) => {
+                    this.setState({
+                        stoks: json
+                    })
+                    console.log("Stocks :", this.state.stoks);
+                })
+                .catch((e) => {
+                    console.log(e)
+                    alert("Failed sending data!!");
+                });
+
+        }
+        this.getCount = () => {
+            fetch(this.state.url + "count", {
+                method: "get",
+                headers: {
+                    "Content-Type": "application/json; ; charset=utf-8",
+                    "Access-Control-Allow-Headers": "Authorization, Content-Type",
+                    "Access-Control-Allow-Origin": "*"
+                }
+            })
+                .then(response => response.json())
+                .then(json => {
+                    this.setState({
+                        count: Math.ceil((json) / this.state.show),
+                        errorFetcing: true
+                    });
+                    console.log(json)
+                })
+                .catch((e) => {
+                    alert("Failed fetching data!!", e)
+                    // this.setState({errorFetcing:true})
+                });
+        }
+        this.getPaging = (value, orderby, show, minus, name) => {
+            if (value) { } else { value = 1; }
+            if (orderby) { } else { orderby = "asc" }
+            if (show) { } else { show = 7 }
+            if (minus) { } else { minus = 0 }
+            if (name) { } else { name = "" }
+            fetch(this.state.url + "paging/?page=" + value + "&limit=" + show + "&orderby=" + orderby + "&minus=" + minus + "&name=" + name, {
+                method: "get",
+                headers: {
+                    "Content-Type": "application/json; ; charset=utf-8",
+                    "Access-Control-Allow-Headers": "Authorization, Content-Type",
+                    "Access-Control-Allow-Origin": "*"
+                }
+            })
+                .then(response => response.json())
+                .then(json => {
+                    this.setState({
+                        stoks: json,
+                        errorFetcing: true
+                    });
+                })
+                .catch((e) => {
+                    alert("Failed fetching data!!", e)
+                    this.setState({ errorFetcing: true })
+                });
+        }
     }
 
     componentDidMount() {
-        this.getApiStock();
+        this.getPaging();
+        this.getCount();
     }
 
     render() {
         const { useStyles } = this.props
         const classes = () => useStyles;
-        const { checkedA } = this.state
+        const { checkedA, show } = this.state
         const result = this.state.stoks.map(
             (value, idx) =>
                 <Tr onClick={() => { this.detail(value.idStok) }} key={idx}>
@@ -153,10 +224,10 @@ class MonitoringStok extends Component {
                     <Td>{value.tanggaDokumen}</Td>
                     <Td>{value.deskripsiDokumen}</Td>
                     <Td>
-                        <b>Total Qty Stock: {value.totalStock}</b><br/>
-                        <b>Count Stock : {value.countStock}</b><br/>
+                        <b>Total Qty Stock: {value.totalStock}</b><br />
+                        <b>Count Stock : {value.countStock}</b><br />
                         <b>Net Amount : {value.netAmount}</b>
-                        </Td>
+                    </Td>
                 </Tr>
         )
         return (
@@ -207,39 +278,42 @@ class MonitoringStok extends Component {
                                     </Button>
                                 </div>
                                 <div data-tip="Delete Stock">
-                                    <Button  onClick={() => { this.delete(this.state.id) }}>
+                                    <Button onClick={() => { this.delete(this.state.id) }}>
                                         <FaIcons.FaTrash />
                                     </Button>
                                 </div>
                             </>
                             : ""
                         }
-                         <div data-tip="Add Stock">
-                        <Button
-                            className="btn-kiri"
-                            onClick={() => {
-                                this.props.history.push("/add-monitoring")
-                            }}
-                        >
-                            <FaIcons.FaPlus />
-                        </Button>
+                        <div data-tip="Add Stock">
+                            <Button
+                                className="btn-kiri"
+                                onClick={() => {
+                                    this.props.history.push("/add-monitoring")
+                                }}
+                            >
+                                <FaIcons.FaPlus />
+                            </Button>
                         </div>
                         <div data-tip="Report Stock">
-                        <Button
-                            onClick={() => {
-                                this.props.history.push("/report-monitoring")
-                            }}
-                        >
-                            <FaIcons.FaBook />
-                        </Button>
+                            <Button
+                                onClick={() => {
+                                    this.props.history.push("/report-monitoring")
+                                }}
+                            >
+                                <FaIcons.FaBook />
+                            </Button>
                         </div>
                         <P className="pM">Show</P>
                         <div data-tip="Show Stock">
-                        <Select>
-                            <Option value="5">5</Option>
-                            <Option value="10">10</Option>
-                            <Option value="15">15</Option>
-                        </Select>
+                            <Select onChange={this.onChangeSelectShow} name="show" value={show}>
+                                <Option value="-">-Pilih-</Option>
+                                <Option value="7">7</Option>
+                                <Option value="10">10</Option>
+                                <Option value="15">15</Option>
+                                <Option value="30">30</Option>
+                                <Option value="50">50</Option>
+                            </Select>
                         </div>
                     </DivClassSingle>
                 </DivClassSingle>
@@ -258,7 +332,7 @@ class MonitoringStok extends Component {
                 </DivClassSingle>
                 <DivClassSingle className="pagination">
                     <DivClassSingle class={classes.root} className="data-pagination" >
-                        <Pagination count={10} color="secondary" variant="outlined" shape="rounded" />
+                        <Pagination count={this.state.count} page={this.state.page} onChange={this.handleChange} />
                     </DivClassSingle>
                 </DivClassSingle>
             </DivClassSingle>
